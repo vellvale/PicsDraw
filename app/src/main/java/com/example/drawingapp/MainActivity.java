6b3c87ed6af1229e54e8f17165afaaa76e1b1868
vellvale <vellvalez@gmail.com>
2021-03-14 02:01:12 +0400

    ctrl+alt+L

--- app/src/main/java/com/example/drawingapp/MainActivity.java
+++ app/src/main/java/com/example/drawingapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import java.util.UUID;
+
 import android.provider.MediaStore;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -15,7 +16,7 @@ import android.content.DialogInterface;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener{
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private DrawingView drawView;
     //Мы устанавливаем начальный цвет краски в классе View для рисования, теперь давайте настроим пользовательский интерфейс,
@@ -25,13 +26,14 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     private float smallBrush, mediumBrush, largeBrush;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //создайте экземпляр drawView, получив ссылку на нее из макета:
-        drawView = (DrawingView)findViewById(R.id.drawing);
+        drawView = (DrawingView) findViewById(R.id.drawing);
         drawView.setBrushSize(mediumBrush);
         //Теперь у нас есть представление, отображаемое в действии, в котором мы можем вызывать методы DrawingView класса.
 
@@ -41,36 +43,36 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
         //мы теперь хотим получить первую кнопку цвета краски в области палитры,
         // которая изначально будет выбрана. Сначала извлеките линейный макет, в котором он содержится
-        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        LinearLayout paintLayout = (LinearLayout) findViewById(R.id.paint_colors);
 
         //Получите первую кнопку и сохраните ее как переменную экземпляра:
-        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint = (ImageButton) paintLayout.getChildAt(0);
         //Мы будем использовать другое изображение на кнопке, чтобы показать, что она в данный момент выбрана
         currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
 
-        drawBtn = (ImageButton)findViewById(R.id.draw_btn);
+        drawBtn = (ImageButton) findViewById(R.id.draw_btn);
         drawBtn.setOnClickListener(this);
 
-        eraseBtn = (ImageButton)findViewById(R.id.erase_btn);
+        eraseBtn = (ImageButton) findViewById(R.id.erase_btn);
         eraseBtn.setOnClickListener(this);
 
-        newBtn = (ImageButton)findViewById(R.id.new_btn);
+        newBtn = (ImageButton) findViewById(R.id.new_btn);
         newBtn.setOnClickListener(this);
 
-        saveBtn = (ImageButton)findViewById(R.id.save_btn);
+        saveBtn = (ImageButton) findViewById(R.id.save_btn);
         saveBtn.setOnClickListener(this);
 
     }
 
-    public void paintClicked(View view){
+    public void paintClicked(View view) {
         drawView.setErase(false);
         //drawView.setBrushSize(drawView.getLastBrushSize());
         //use chosen color
         //сначала убедитесь, что пользователь щелкнул цвет краски, который не является текущим выбранным
-        if(view!=currPaint){
+        if (view != currPaint) {
 //update color
             //извлеките тег, который мы установили для каждой кнопки в макете, представляющий выбранный цвет
-            ImageButton imgView = (ImageButton)view;
+            ImageButton imgView = (ImageButton) view;
             String color = view.getTag().toString();
 
 //после получения тега цвета вызовите новый метод для настраиваемого объекта просмотра чертежа:
@@ -80,24 +82,24 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 //Теперь обновите пользовательский интерфейс, чтобы отразить новую выбранную краску, и верните предыдущую в нормальное состояние
             imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
             currPaint.setImageDrawable(getResources().getDrawable(R.drawable.drawable));
-            currPaint=(ImageButton)view;
+            currPaint = (ImageButton) view;
 
         }
 
     }
 
     @Override
-    public void onClick(View view){
+    public void onClick(View view) {
 //respond to clicks
-        if(view.getId()==R.id.draw_btn){
+        if (view.getId() == R.id.draw_btn) {
             //draw button clicked
             final Dialog brushDialog = new Dialog(this);
             brushDialog.setTitle("Размер кисти:");
             brushDialog.setContentView(R.layout.brush_chooser);
 
 
-            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
-            smallBtn.setOnClickListener(new OnClickListener(){
+            ImageButton smallBtn = (ImageButton) brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     drawView.setBrushSize(smallBrush);
@@ -106,8 +108,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                     brushDialog.dismiss();
                 }
             });
-            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
-            mediumBtn.setOnClickListener(new OnClickListener(){
+            ImageButton mediumBtn = (ImageButton) brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     drawView.setBrushSize(mediumBrush);
@@ -117,8 +119,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                 }
             });
 
-            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
-            largeBtn.setOnClickListener(new OnClickListener(){
+            ImageButton largeBtn = (ImageButton) brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     drawView.setBrushSize(largeBrush);
@@ -128,15 +130,14 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                 }
             });
             brushDialog.show();
-        }
-        else if(view.getId()==R.id.erase_btn){
+        } else if (view.getId() == R.id.erase_btn) {
             //switch to erase - choose size
             final Dialog brushDialog = new Dialog(this);
             brushDialog.setTitle("Eraser size:");
             brushDialog.setContentView(R.layout.brush_chooser);
 
-            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
-            smallBtn.setOnClickListener(new OnClickListener(){
+            ImageButton smallBtn = (ImageButton) brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     drawView.setErase(true);
@@ -144,8 +145,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                     brushDialog.dismiss();
                 }
             });
-            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
-            mediumBtn.setOnClickListener(new OnClickListener(){
+            ImageButton mediumBtn = (ImageButton) brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     drawView.setErase(true);
@@ -153,8 +154,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                     brushDialog.dismiss();
                 }
             });
-            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
-            largeBtn.setOnClickListener(new OnClickListener(){
+            ImageButton largeBtn = (ImageButton) brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     drawView.setErase(true);
@@ -163,54 +164,53 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                 }
             });
             brushDialog.show();
-        }
-        else if(view.getId()==R.id.new_btn){
+        } else if (view.getId() == R.id.new_btn) {
             //new button
             AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
             newDialog.setTitle("Новый холст");
             newDialog.setMessage("Вы уверены, что хотите очистить холст?");
-            newDialog.setPositiveButton("Да", new DialogInterface.OnClickListener(){
-                public void onClick(DialogInterface dialog, int which){
+            newDialog.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
                     drawView.startNew();
                     dialog.dismiss();
                 }
             });
-            newDialog.setNegativeButton("Отмена", new DialogInterface.OnClickListener(){
-                public void onClick(DialogInterface dialog, int which){
+            newDialog.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
                     dialog.cancel();
                 }
             });
             newDialog.show();
-        }
-        else if(view.getId()==R.id.save_btn){
-            drawView.setDrawingCacheEnabled(true);
-            String imgSaved = MediaStore.Images.Media.insertImage(
-                    getContentResolver(), drawView.getDrawingCache(),
-                    UUID.randomUUID().toString()+".png", "drawing");
-            if(imgSaved!=null){
-                Toast savedToast = Toast.makeText(getApplicationContext(),
-                        "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
-                savedToast.show();
-            }
-            else{
-                Toast unsavedToast = Toast.makeText(getApplicationContext(),
-                        "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
-                unsavedToast.show();
-            }
-
-            drawView.destroyDrawingCache();
+        } else if (view.getId() == R.id.save_btn) {
 
             //save drawing
             AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
             saveDialog.setTitle("Сохранение рисунка");
             saveDialog.setMessage("Сохранить рисунок в галерее устройства?");
-            saveDialog.setPositiveButton("Да", new DialogInterface.OnClickListener(){
-                public void onClick(DialogInterface dialog, int which){
+            saveDialog.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
                     //save drawing
+                    drawView.setDrawingCacheEnabled(true);
+
+                    String imgSaved = MediaStore.Images.Media.insertImage(
+                            getContentResolver(), drawView.getDrawingCache(),
+                            UUID.randomUUID().toString() + ".png", "drawing");
+
+                    if (imgSaved != null) {
+                        Toast savedToast = Toast.makeText(getApplicationContext(),
+                                "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                        savedToast.show();
+                    } else {
+                        Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                                "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                        unsavedToast.show();
+                    }
+
+                    drawView.destroyDrawingCache();
                 }
             });
-            saveDialog.setNegativeButton("Отмена", new DialogInterface.OnClickListener(){
-                public void onClick(DialogInterface dialog, int which){
+            saveDialog.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
                     dialog.cancel();
                 }
             });
